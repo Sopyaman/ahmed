0d905454ede67facd1666ad5942c9bfb003a2e69
Sopyaman <am2724689@gmail.com>
2024-12-27 04:13:26 -0800

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+name = input("inter a name: ")
+game = input("inter a game:  ")  
+gmae2 = input("inter game in frist game you write: ")
+print("ahmed is a "+name)
+print("ahmed love "+game)
+print("ahmed play "+gmae2)
